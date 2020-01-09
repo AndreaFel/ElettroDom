efaa7084d2a965f346303b7e90da071239d0ec58
AndreaFel <and.felline@gmail.com>
2020-01-09 15:02:18 +0100

    aggiunti "std::" e la libreria "iostream"

--- Componente.h
+++ Componente.h
@@ -1,9 +1,10 @@
+#include <iostream>
 
 class componente{
 	
 public:
 	//costruttore: alla creazione vengono settati l'id, il nome e i 3 prezzi (per l'acquisto di 1-10, 11-50 e 51+ pezzi)
-	componente(string id, string nome, float prezzi[]);
+	componente(std::string id, std::string nome, float prezzi[]);
 	
 	//get
 	Prezzo(int pezzi);//ritorna il prezzo in base al numero di pezzi
@@ -11,7 +12,7 @@ public:
 	Id();
 	
 private:
-	string id;
-	string nome;
+	std::string id;
+	std::string nome;
 	float prezzi[];
-}
+};
