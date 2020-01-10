116bc4bb425880dd4be70a32708e3c1176e2834c
AndreaFel <and.felline@gmail.com>
2020-01-10 19:05:15 +0100

    aggiunti mesi di arrivo e "const" dove serve

--- Componente.h
+++ Componente.h
@@ -4,15 +4,16 @@ class componente{
 	
 public:
 	//costruttore: alla creazione vengono settati l'id, il nome e i 3 prezzi (per l'acquisto di 1-10, 11-50 e 51+ pezzi)
-	componente(std::string id, std::string nome, float prezzi[]);
+	componente(std::string id, std::string nome, int mesi, double prezzi[]);
 	
 	//get
-	Prezzo(int pezzi);//ritorna il prezzo in base al numero di pezzi
-	Nome();
-	Id();
-	
+	getPrezzo(int pezzi) const;//ritorna il prezzo in base al numero di pezzi
+	getNome() const;
+	getId() const;
+	getMesi() const;
 private:
 	std::string id;
 	std::string nome;
-	float prezzi[];
+	int mesi;
+	double prezzi[];
 };
