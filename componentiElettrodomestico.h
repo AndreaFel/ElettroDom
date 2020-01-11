037954030fb201f9749598a0a765391ff6533621
EdoardoBasti <59654237+EdoardoBasti@users.noreply.github.com>
2020-01-11 21:45:57 +0100

    aggiunta const reference nel costruttore

--- componentiElettrodomestico.h
+++ componentiElettrodomestico.h
@@ -1,15 +1,20 @@
-//un oggetto di tipo componentiElettrodomestico servira' per rappresentare un oggetto di tipo elettrodomestico
+//Autore: Edoardo Bastianello , numero matricola: 1188629
+//header "componentiElettrodomestico.h"
+//un oggetto di tipo componentiElettrodomestico servira' per realizzare la classe elettrodomestico
 
 class componentiElettrodomestico{
 
 public:
-	componentiElettrodomestico(componente in, int num);
+	//costruttore vuoto non definito
+	//costruttore, "num" rappresenta la quantità necessaria di un certo componente "in" per realizzare il rispettivo elettrodomestico
+	componentiElettrodomestico(const componente& in, int num);
 	
 	//funzioni di accesso
 	componente getComponente()const;
 	int getPezzi()const;
 
 private:
+	//dati membro
 	componente c;
 	int pezzi;
 };
