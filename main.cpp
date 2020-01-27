fd906e39364d4e3ea6363a693ebb1ec4061af3df
AndreaFel <and.felline@gmail.com>
2020-01-27 18:11:37 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <gestione.h>
+
+using namespace std;
+
+int main(){
+	Gestione ElettroDom {};
+	
+	while(ElettroDom.getInAttesa()){
+		
+		if(ElettroDom.aggiornaMese())
+			cout<<ElettroDom.stampaStato()<<endl;
+		else
+			cout<<"Nessun ordine evaso\n";
+		
+		cout<<"\n\n\t\tPassa un mese\n\n";
+	}
+	return 0;
+}
